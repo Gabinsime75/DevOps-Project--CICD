496a44058300c1e305221f4361f3621d82c4470f
Gabin Sime <gabin75@yahoo.de>
2023-09-02 13:30:32 -0700

    updated tomcat.sh file

--- tomcat_install.sh
+++ tomcat_install.sh
@@ -2,13 +2,7 @@ sudo su -
 amazon-linux-extras install java-openjdk11
 java --version
 cd /opt
-
-# wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.78/bin/apache-tomcat-9.0.78.tar.gz
-# wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.13/bin/apache-tomcat-10.1.13.tar.gz
-wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.13/bin/apache-tomcat-10.1.13-deployer.tar.gz
-
-# tar -xvzf apache-tomcat-9.0.78.tar.gz 
-# tar -xvzf apache-tomcat-10.1.13.tar.gz 
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.13/bin/apache-tomcat-10.1.13.tar.gz
 tar -xzvf apache-tomcat-10.1.13-deployer.tar.gz
 mv apache-tomcat-10.1.13 tomcat
 cd tomcat/bin
